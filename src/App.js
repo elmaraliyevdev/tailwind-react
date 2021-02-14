6f2da6bc6672161a8845a0101d70ab89e8e7563f
Elmar Aliyev <elmarnaliyev@gmail.com>
2021-02-14 14:56:20 +0400

    Tailwind CSS React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="text-gray-600 body-font">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <a href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        <span className="ml-3 text-xl">Tailwind React</span>
+                    </a>
+                    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                        <a href="/" className="mr-5 hover:text-gray-900">
+                            First Link
+                        </a>
+                        <a href="/" className="mr-5 hover:text-gray-900">
+                            Second Link
+                        </a>
+                        <a href="/" className="mr-5 hover:text-gray-900">
+                            Third Link
+                        </a>
+                        <a href="/" className="mr-5 hover:text-gray-900">
+                            Fourth Link
+                        </a>
+                    </nav>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
